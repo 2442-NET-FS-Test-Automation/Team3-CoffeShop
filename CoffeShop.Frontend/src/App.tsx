b844fe6210c4906fc7dfb9cd90220853a0596af2
Ignacio Gomez Gracian <ignaciogmz99@gmail.com>
2026-07-20 10:39:20 -0600

    Navbar with buttons

--- CoffeShop.Frontend/src/App.tsx
+++ CoffeShop.Frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar.tsx'
 
 function App() {
   return (
-    <main className="app">
-      <h1>Hello World</h1>
-    </main>
+      <div className='App'>
+        <Navbar />
+        <main className="app">
+        <h1>Hello World</h1>
+        </main>
+      </div>
+      
   )
 }
 
---
+++ CoffeShop.Frontend/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return(
+        <nav className="navbar">
+            <div className = "nav-logo">CoffeShopRevanture</div>
+            <div className = "nav-buttons">
+                <button className="login button">Login</button>
+                <button className="register button">Register</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
