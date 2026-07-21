7968d2af1c8f52374d03fff92418581e84b436bc
Ignacio Gomez Gracian <ignaciogmz99@gmail.com>
2026-07-21 14:59:06 -0600

    frotend navbar and login page

--- CoffeShop.Frontend/src/Components/Navbar/Navbar.tsx
+++ CoffeShop.Frontend/src/Components/Navbar/Navbar.tsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
 import './Navbar.css';
+import logo from '../../assets/CoffeShopLogo.png';
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return(
+    return (
         <nav className="navbar">
-            <div className = "nav-logo">CoffeShopRevanture</div>
-            <div className = "nav-buttons">
+            <div className="nav-brand">
+                <img className="nav-logo" src={logo} alt="Company Logo" />
+                <span className="nav-title">Coffe Shop Revanture</span>
+            </div>
+
+            <div className="nav-menu">
+                <button className="nav-link">Menu</button>
+                <button className="nav-link">Orders</button>
+                <button className="nav-link">Inventory</button>
+            </div>
+
+            <div className="nav-actions">
                 <button className="login button">Login</button>
                 <button className="register button">Register</button>
             </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
